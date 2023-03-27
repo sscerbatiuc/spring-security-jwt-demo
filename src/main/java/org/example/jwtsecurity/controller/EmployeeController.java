410518a95feaebfa7b3b5c1c287a9c52e33479cb
sscerbatiuc <s.scerbatiuc@jdi.nl>
2023-03-27 17:54:33 +0300

    Added example controller

---
+++ src/main/java/org/example/jwtsecurity/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package org.example.jwtsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @GetMapping
+    public String getAll() {
+        return "Hello";
+    }
+}
